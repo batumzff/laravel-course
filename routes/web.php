599e2727922582dce9718a4b884e4826ab442ddb
batumzff <batuhanmuzafferoglu@gmail.com>
2024-11-06 17:47:55 +0300

    controller with apiResources

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ShowCarController;
+// use App\Http\Controllers\CarController;
+// use App\Http\Controllers\ShowCarController;
+// use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,13 +18,22 @@ Route::get('/', function () {
 
 Route::view('/about','about')->name('about');
 
+
+// Route::apiResources([
+//     'cars'=> CarController::class,
+//     'product' => ProductController::class
+// ]);
+
+
+
+
 // Route::controller (  CarController::class)->group (function() {
 //     Route::get( '/car',  'index');
 //     Route::get( '/my-cars', 'myCars');
 //     });
 
-Route::get('/car/invokable',CarController::class);
-Route::get('/car',[CarController::class, 'index']);
+// Route::get('/car/invokable',CarController::class);
+// Route::get('/car',[CarController::class, 'index']);
 
 
 
