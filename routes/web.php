78b323673030eb5b4899b3c53bfae7e7f3289e89
batumzff <batuhanmuzafferoglu@gmail.com>
2024-11-15 19:05:02 +0300

    added: login page

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,14 @@
 
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/signup', [SignupController::class, 'create']);
+Route::get('/login', [LoginController::class, 'create']);
 
 
 // Route::view('/about','about')->name('about');
