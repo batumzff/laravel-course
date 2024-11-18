0a5f225902dba521190c98a79cad75877631a4e5
batumzff <batuhanmuzafferoglu@gmail.com>
2024-11-18 22:17:11 +0300

    update footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,11 @@
   @section('childContent')
     @include('layouts.partials.header')
     @yield('content')
+    @hasSection('footerLinks')
     <footer>
-      @section('footerLinks')
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-      @show
-    </footer>
+      @yield('footerLinks')
+    </footer>  
+    @endif
   @endsection
 
    
