499b9622344a25e4d320dee8e79ffaba6c80b348
batumzff <batuhanmuzafferoglu@gmail.com>
2024-11-27 18:18:12 +0300

    updated: car card

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,14 +1,5 @@
-<x-base-layout title="Signup" bodyClass="page-signup">
-<main>
-      <div class="container-small page-login">
-        <div class="flex" style="gap: 5rem">
-          <div class="auth-page-form">
-            <div class="text-center">
-              <a href="/">
-                <img src="/img/logoipsum-265.svg" alt="" />
-              </a>
-            </div>
-            <h1 class="auth-page-title">Signup</h1>
+<x-guest-layout title="Signup" bodyClass="page-signup">
+  <h1 class="auth-page-title">Signup</h1>
 
             <form action="" method="post">
               <div class="form-group">
@@ -31,22 +22,13 @@
                 <input type="text" placeholder="Phone" />
               </div>
               <button class="w-full btn btn-primary btn-login">Register</button>
+            </form>
+              
+              
+            
 
-              <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-                <x-google-button />
-                <x-fb-button />
-                
-              </div>
-              <div class="login-text-dont-have-account">
+            <x-slot:footerLink>
                 Already have an account? -
                 <a href="/login.html"> Click here to login </a>
-              </div>
-            </form>
-          </div>
-          <div class="auth-page-image">
-            <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
-          </div>
-        </div>
-      </div>
-    </main>
-</x-base-layout>
+            </x-slot:footerLink>
+</x-guest-layout>
