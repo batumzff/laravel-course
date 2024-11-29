26dc85010b5b3bd62da33f881f784ff975d4d350
batumzff <batuhanmuzafferoglu@gmail.com>
2024-11-30 00:03:37 +0300

    added: controllers

---
+++ app/Models/FuelType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+    use HasFactory;
+
+    // protected $table = 'car_fuel_types';
+    // protected $primaryKey = 'fuel_type_id';
+    // public $incrementing = false;
+    // protected $keyType = 'string';
+    // const UPDATED_AT = null;
+    
+    public $timestamps = false;
+    
+}
