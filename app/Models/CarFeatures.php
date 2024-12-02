e445da12dfc9e39bbbd30d409fb7f2b8673e9aa6
batumzff <batuhanmuzafferoglu@gmail.com>
2024-12-02 18:54:17 +0300

    added: models

--- app/Models/CarFeatures.php
+++ app/Models/CarFeatures.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CarFeatures extends Model
 {
-    //
+    use HasFactory;
+
+    public $timesstamps = false;
 }
--- app/Models/State.php
+++ app/Models/State.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    use HasFactory;
+
+    public $timesstamps = false;
 }
