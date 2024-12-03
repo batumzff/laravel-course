a0fb86034d9aa48a26a301ca5773412a844a4a25
batumzff <batuhanmuzafferoglu@gmail.com>
2024-12-03 16:06:39 +0300

    filled the tables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        // // Seleect All Cars
+        // $cars = Car::get();
+
+        // // Select Published Cars
+        // $cars = Car::where('published_at', '!=', null)->get();
+
+        // // Select the first car
+        // $car = Car::where('published_at', '!=', null)->first();
+
+        $car = Car::orderBy('published_at', 'desc')->limit(2)->get();
+
+        dump($car);
+
         return view('home.index');
     }
 }
